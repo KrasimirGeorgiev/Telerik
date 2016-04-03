d1617e1abe02342564a530e78a7bbc3d05f0c138
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-04-03 09:40:26 +0300

    ConsoleInputOutput
    Added numberComparer exercise

--- 01. C# Fundamentals/04. ConsoleInAndOut/homework/ConsoleInputOutput/05. NumbersComparer/NumbersComparer.cs
+++ 01. C# Fundamentals/04. ConsoleInAndOut/homework/ConsoleInputOutput/05. NumbersComparer/NumbersComparer.cs
@@ -1,15 +1,31 @@
 ﻿namespace _05.NumbersComparer
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class NumbersComparer
     {
         public static void Main()
         {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double maxNumber = Math.Max(a, b);
+
+            Console.WriteLine(maxNumber);
+
+            //// Input  Output
+            //// 5
+            //// 6      6
+            //// 10
+            //// 5      10
+            //// 0
+            //// 0      0
+            //// - 5
+            //// - 2    - 2
+            //// 1.5
+            //// 1.6    1.6
         }
     }
 }
+
+// Write a program that gets two numbers from the console and prints the greater of them.
+// Try implementing it without using if-statements
