90244ef017ab8ad4be51a58e1a1a8da38e46179e
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-04-07 17:46:31 +0300

    Exam Preparation
    One exercise

---
+++ 01. C# Fundamentals/ExamPreparation/ExamPreparation/01. ThreeNumbers/ThreeNumbers.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.ThreeNumbers
+{
+    using System;
+
+    public class ThreeNumbers
+    {
+        public static void Main()
+        {
+            decimal numberA = decimal.Parse(Console.ReadLine());
+            decimal numberB = decimal.Parse(Console.ReadLine());
+            decimal numberC = decimal.Parse(Console.ReadLine());
+            decimal max = Math.Max(Math.Max(numberA, numberB), numberC);
+            decimal min = Math.Min(Math.Min(numberA, numberB), numberC);
+            decimal arithmeticMean = (numberA + numberB + numberC) / 3;
+
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+            Console.WriteLine("{0:F2}", arithmeticMean);
+        }
+    }
+}
+
+// Telerik Academy Exam 1 @ 3 February 2015 Morning
+// http://bgcoder.com/Contests/Practice/Index/204#0
---
+++ 01. C# Fundamentals/ExamPreparation/ExamPreparation/02. Decoding/Decoding.cs
@@ -0,0 +1,18 @@
+﻿namespace _02.Decoding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Decoding
+    {
+        public static void Main()
+        {
+        }
+    }
+}
+
+// Telerik Academy Exam 1 @ 3 February 2015 Morning
+// http://bgcoder.com/Contests/Practice/Index/204#1
