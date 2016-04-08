fe24099d21da2da4a7194c3e0d9e32bdb7ff20a1
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-04-08 20:22:53 +0300

    Loops homework
    Started exercise "Spiral Matrix"

--- 01. C# Fundamentals/06. Loops/homework/Loops/17. SpiralMatrix/SpiralMatrix.cs
+++ 01. C# Fundamentals/06. Loops/homework/Loops/17. SpiralMatrix/SpiralMatrix.cs
@@ -8,23 +8,46 @@
 
     public class SpiralMatrix
     {
+        static int n = int.Parse(Console.ReadLine());
+        static int[,] spiralMatrix = new int[n, n];
+        static int count = 1;
+        static int limitation = 0;
+        static int row = 0;
+        static int col = 0;
+        static char direction = 'r';
+
         public static void Main()
         {
-            int n = int.Parse(Console.ReadLine());
-            int[,] spiralMatrix = new int[n, n];
-            int count = 1;
-            int restrictionCount = 0;
+            
 
-            // for (int i = 0; i < length; i++)
-            // {
-            //     for (int i = 0; j < length; i++)
-            //     {
-            // 
-            //     }
-            // }
+            while (true)
+            {
+                switch (direction)
+                {
+                    case 'r': break;
+                    case 'd': break;
+                    case 'l': break;
+                    case 'u': break;
+                }
+            }
+        }
+
+        private static void Right()
+        {
+            for (int i = col; i < n - limitation; i++)
+            {
+                spiralMatrix[row, col] = count;
+                count++;
+                if (i == (n - limitation) - 1)
+                {
+                    col = i;
+                }
+            }
+
+            direction = 'd';
         }
     }
 }
 
-// Write a program that reads from the console a positive integer number N(1 ≤ N ≤ 20) and prints a matrix holding the numbers from 1 to N* N in the 
+// Write a program that reads from the console a positive integer number N(1 ≤ N ≤ 20) and prints a matrix holding the numbers from 1 to N * N in the 
 // form of square spiral like in the examples below.
