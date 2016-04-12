69987323b9f8da853f8f9b50ba5397166b7dfe60
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-04-12 12:14:24 +0300

    C# exam prep
    Finished task 02. Encoding

--- 01. C# Fundamentals/ExamPreparation/ExamPreparation/02. Decoding/Decoding.cs
+++ 01. C# Fundamentals/ExamPreparation/ExamPreparation/02. Decoding/Decoding.cs
@@ -1,15 +1,48 @@
 ﻿namespace _02.Decoding
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class Decoding
     {
         public static void Main()
         {
+            int salt = int.Parse(Console.ReadLine());
+            string str = Console.ReadLine();
+            int strLength = str.Length;
+
+            for (int i = 0; i < strLength; i++)
+            {
+                char currentChar = str[i];
+                decimal result = 0;
+                if (currentChar == '@')
+                {
+                    break;
+                }
+
+                if (char.IsLetter(currentChar))
+                {
+                    result = currentChar * salt + 1000M;
+                }
+                else if(char.IsDigit(currentChar)) // !!! If not working try manualy to specify only Latin latters
+                {
+                    result = currentChar + salt + 500M;
+                }
+                else
+                {
+                    result = currentChar - (decimal)salt;
+                }
+
+                if (i % 2 == 0)
+                {
+                    result /= 100;
+                    Console.WriteLine("{0:F2}", result);
+                }
+                else
+                {
+                    result *= 100;
+                    Console.WriteLine(result);
+                }
+            }
         }
     }
 }
