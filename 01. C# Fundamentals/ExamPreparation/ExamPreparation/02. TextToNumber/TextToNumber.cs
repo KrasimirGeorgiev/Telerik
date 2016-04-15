4cef09e328a41b8b7f651a2d59dd98dd299ac8d5
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-04-15 16:52:21 +0300

    Exam prep
    Added exercise 02. TextToNumber - not finished

---
+++ 01. C# Fundamentals/ExamPreparation/ExamPreparation/02. TextToNumber/TextToNumber.cs
@@ -0,0 +1,42 @@
+﻿namespace _02.TextToNumber
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TextToNumber
+    {
+        public static void Main()
+        {
+            int m = int.Parse(Console.ReadLine());
+            string str = Console.ReadLine();
+            long result = 0;
+
+            foreach (char symbol in str)
+            {
+                if (symbol == '@')
+                {
+                    break;
+                }
+
+                if (char.IsDigit(symbol))
+                {
+                    result = result == 0 ? 1 : result;
+                    result *= symbol - '0';
+                }
+                else if (char.IsLetter(symbol))
+                {
+                    result += char.ToLower(symbol) - 'a';
+                }
+                else
+                {
+                    result %= m;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
