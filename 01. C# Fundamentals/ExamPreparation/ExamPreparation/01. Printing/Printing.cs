abe8bb458ae31681482fa4223b6ac08be9c6bee1
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-04-15 15:52:19 +0300

    Exam prep
    Added exercise 01. Printing

---
+++ 01. C# Fundamentals/ExamPreparation/ExamPreparation/01. Printing/Printing.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.Printing
+{
+    using System;
+
+    public class Printing
+    {
+        public static void Main()
+        {
+            decimal n = decimal.Parse(Console.ReadLine());
+            decimal s = decimal.Parse(Console.ReadLine());
+            decimal p = decimal.Parse(Console.ReadLine());
+            decimal result = (((decimal)n * s) / 500M) * p;
+
+            Console.WriteLine("{0:F2}", result);
+        }
+    }
+}
+
+// Telerik Academy Exam 1 @ 2 February 2015 Morning
+// http://bgcoder.com/Contests/Practice/Index/202#0
