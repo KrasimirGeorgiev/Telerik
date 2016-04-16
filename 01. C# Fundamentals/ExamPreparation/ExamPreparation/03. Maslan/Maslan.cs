f461233a4fadf5a2e4eef723d21e28aeb61904f0
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-04-16 15:37:01 +0300

    Exam prep
    no real thing happened that day, but the show must go on...

---
+++ 01. C# Fundamentals/ExamPreparation/ExamPreparation/03. Maslan/Maslan.cs
@@ -0,0 +1,15 @@
+﻿namespace _03.Maslan
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Maslan
+    {
+        public static void Main()
+        {
+        }
+    }
+}
