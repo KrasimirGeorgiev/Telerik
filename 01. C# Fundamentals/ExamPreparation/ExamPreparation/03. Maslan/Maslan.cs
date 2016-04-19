e7ed44988120dbe4581687496a899df30285ac08
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-04-19 10:41:05 +0300

    Exam prep
    Added exercise 03. Maslan

--- 01. C# Fundamentals/ExamPreparation/ExamPreparation/03. Maslan/Maslan.cs
+++ 01. C# Fundamentals/ExamPreparation/ExamPreparation/03. Maslan/Maslan.cs
@@ -1,15 +1,64 @@
 ﻿namespace _03.Maslan
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.Numerics;
 
     public class Maslan
     {
         public static void Main()
         {
+            string n = Console.ReadLine();
+            string number = n;
+            int countTransformations = 0;
+
+            while (true)
+            {
+                BigInteger product = 1;
+                number = number.Substring(0, number.Length - 1);
+                while (number.Length > 0)
+                {
+                    BigInteger currentNumber = 0;
+                    currentNumber = SumOfOddDigits(number);
+                    if (currentNumber != 0)
+                    {
+                        product *= currentNumber;
+                    }
+
+                    number = number.Substring(0, number.Length - 1);
+                }
+
+                number = product.ToString();
+                countTransformations++;
+                if (number.Length == 1)
+                {
+                    Console.WriteLine(countTransformations);
+                    Console.WriteLine(number);
+                    break;
+                }
+
+                if (countTransformations == 10)
+                {
+                    Console.WriteLine(number);
+                    break;
+                }
+            }
+        }
+
+        private static BigInteger SumOfOddDigits(string str)
+        {
+            BigInteger result = 0;
+            int count = 0;
+            foreach (char symbol in str)
+            {
+                if (count % 2 != 0)
+                {
+                    result += symbol - '0';
+                }
+
+                count++;
+            }
+
+            return result;
         }
     }
 }
