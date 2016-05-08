c7e0d7f9c890b49ed7b9757468a176cb41b36611
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-05-08 23:03:14 +0300

    CSharpAdvanced

---
+++ 02. C# Advanced/ExamPreparation/CSharpPart2ExamPreparation/Tests/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            long input = long.Parse(Console.ReadLine());
+            Console.WriteLine(DecimalToCatNumeral(input));
+        }
+
+        private static string DecimalToCatNumeral(long number)
+        {
+            Stack<string> reverseResult = new Stack<string>();
+            long inputNumber = number;
+            string result = "";
+
+            while (inputNumber != 0)
+            {
+                long remainingNumber = inputNumber % 23;
+                string remNumToCatNum = ((char)(remainingNumber + 'a')).ToString();
+                inputNumber /= 23;
+                reverseResult.Push(remNumToCatNum);
+            }
+
+            while (reverseResult.Count > 0)
+            {
+                string currentLetter = reverseResult.Pop();
+                result += currentLetter;
+            }
+
+            return result;
+        }
+    }
+}
