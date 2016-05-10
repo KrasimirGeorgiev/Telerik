474373332bd23ed00fc0c4a891e0608b851ffa45
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-05-10 11:08:50 +0300

    Advanced C# 01. Arrays homework
    Added exercise 03. ComparingCharArrays

---
+++ 02. C# Advanced/01. Arrays/homework/Arrays/03. CompareCharArrays/CompareCharArrays.cs
@@ -0,0 +1,59 @@
+﻿namespace _03.CompareCharArrays
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CompareCharArrays
+    {
+        public static void Main()
+        {
+            char[] firstArr = Console.ReadLine().ToCharArray();
+            char[] secondArr = Console.ReadLine().ToCharArray();
+            Console.WriteLine(CompareCharArraysLexicographically(firstArr, secondArr));
+        }
+
+        private static char CompareCharArraysLexicographically(char[] firstArr, char[] secondArr) // Can have a problem with Uppercase and Lowercase letters
+        {
+            if (firstArr.Length < secondArr.Length)
+            {
+                return '<';
+            }
+            else if (secondArr.Length < firstArr.Length)
+            {
+                return '>';
+            }
+            else
+            {
+                for (int i = 0; i < firstArr.Length; i++)
+                {
+                    char firstArrCurrentChar = firstArr[i];
+                    char secondArrCurrentChar = firstArr[i];
+                    if (char.IsLetter(firstArrCurrentChar))
+                    {
+                        firstArrCurrentChar = char.ToLower(firstArrCurrentChar);
+                    }
+                    if (char.IsLetter(secondArrCurrentChar))
+                    {
+                        secondArrCurrentChar = char.ToLower(secondArrCurrentChar);
+                    }
+
+                    if (firstArrCurrentChar > secondArrCurrentChar)
+                    {
+                        return '>';
+                    }
+                    else if (secondArrCurrentChar > firstArrCurrentChar)
+                    {
+                        return '<';
+                    }
+                }
+
+                return '=';
+            }
+        }
+    }
+}
+
+// Write a program that compares two char arrays lexicographically (letter by letter).
