8659e16318fce9e4c0d3add23724a2d2ca195373
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-05-10 22:25:27 +0300

    Arrays homework
    Added exercise 09. FrequentNumber

---
+++ 02. C# Advanced/01. Arrays/homework/Arrays/09. FrequentNumber/FrequentNumber.cs
@@ -0,0 +1,53 @@
+﻿namespace _09.FrequentNumber
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class FrequentNumber
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal[] arr = new decimal[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = decimal.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(FindMostFrequentNumber(arr));
+        }
+
+        private static decimal FindMostFrequentNumber(decimal[] arr)
+        {
+            //arr = arr.OrderBy(x => x);
+            int maxCount = 0;
+            decimal maxNumber = 0M;
+            int currentCount = 0;
+            decimal currentMaxNumber = 0M;
+            while (arr.Length > 0)
+            {
+                currentMaxNumber = arr[0];
+                foreach (decimal number in arr)
+                {
+                    currentCount = number == currentMaxNumber ? currentCount++ : currentCount; 
+                }
+
+                if (maxCount < currentCount)
+                {
+                    maxCount = currentCount;
+                    maxNumber = currentMaxNumber;
+                }
+
+                currentCount = 0;
+                arr = arr.Where((val, idx) => val != currentMaxNumber).ToArray();
+            }
+
+            return maxCount;
+        }
+    }
+}
+
+// Write a program that finds the most frequent number in an array of N elements.
