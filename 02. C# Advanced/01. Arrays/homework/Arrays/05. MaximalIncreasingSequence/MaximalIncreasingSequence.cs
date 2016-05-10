a0ef307099c829f4e2ceea4c78a90530bf1c9963
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-05-10 12:36:35 +0300

    Advanced C# 01. Arrays homework
    Added exercise 05. MaximalIncreasingSequence

---
+++ 02. C# Advanced/01. Arrays/homework/Arrays/05. MaximalIncreasingSequence/MaximalIncreasingSequence.cs
@@ -0,0 +1,56 @@
+﻿namespace _05.MaximalIncreasingSequence
+{
+    using System;
+
+    public class MaximalIncreasingSequence
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(FindLengthOfMaximalIncreasingSequence(arr));
+        }
+
+        private static int FindLengthOfMaximalIncreasingSequence(int[] arr)
+        {
+            if (arr.Length < 1)
+            {
+                return -1;
+            }
+
+            int maxCount = 1;
+            int currentCount = 1;
+            int currentElement = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int currentNumber = arr[i];
+                if (currentNumber > currentElement)
+                {
+                    currentCount++;
+                }
+
+                if (currentNumber <= currentElement || i == arr.Length - 1)
+                {
+                    if (currentCount > maxCount)
+                    {
+                        maxCount = currentCount;
+                    }
+
+                    currentElement = currentNumber;
+                    currentCount = 1;
+                }
+
+                currentElement = currentNumber;
+            }
+
+            return maxCount;
+        }
+    }
+}
+
+// Write a program that finds the length of the maximal increasing sequence in an array of N integers.
