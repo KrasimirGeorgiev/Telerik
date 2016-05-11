a3d422de0b56a5545a368bac4e5d037b925a2ca5
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-05-11 15:14:24 +0300

    Arrays homework
    Added exercise 12. IndexOfLetters

---
+++ 02. C# Advanced/01. Arrays/homework/Arrays/12. IndexOfLetters/IndexOfLetters.cs
@@ -0,0 +1,23 @@
+﻿namespace _12.IndexOfLetters
+{
+    using System;
+
+    public class IndexOfLetters
+    {
+        public static void Main()
+        {
+            string str = Console.ReadLine();
+            foreach (char symbol in str)
+            {
+                if (char.IsLetter(symbol))
+                {
+                    Console.WriteLine(char.ToLower(symbol) - 'a');
+                }
+            }
+        }
+    }
+}
+
+// Write a program that creates an array containing all letters from the alphabet (a-z). 
+// Read a word from the console and print the index of each of its letters in the array.
+// Personal notes: The task is about using array's, but i find it stuped in this case...
