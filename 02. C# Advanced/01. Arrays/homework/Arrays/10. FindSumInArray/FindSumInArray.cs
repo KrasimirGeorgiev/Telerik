73b93e7a6248d40132c75586197a3d25588cabb8
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-05-12 15:47:16 +0300

    Arrays homework
    Added exercise 10. FindSumInArray (Recursively finds all sums in array)

--- 02. C# Advanced/01. Arrays/homework/Arrays/10. FindSumInArray/FindSumInArray.cs
+++ 02. C# Advanced/01. Arrays/homework/Arrays/10. FindSumInArray/FindSumInArray.cs
@@ -3,13 +3,40 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class FindSumInArray
     {
+        private static int numberSum;
+        private static List<int> numbers;
+        private static bool thereIsResult;
+
         public static void Main()
         {
+            int sum = int.Parse(Console.ReadLine());
+            numberSum = sum;
+            numbers = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> subset = new List<int>();
+            CalculateSubset(0, subset);
+            if (!thereIsResult)
+            {
+                Console.WriteLine("No matching subsets.");
+            }
+        }
+        
+        private static void CalculateSubset(int startingIndex, List<int> numbersForSum)
+        {
+            if (numbersForSum.Count > 0 && numbersForSum.Sum() == numberSum)
+            {
+                Console.WriteLine(string.Join(", ", numbersForSum));
+                thereIsResult = true;
+            }
+
+            for (int index = startingIndex; index < numbers.Count - 1; index++)
+            {
+                numbersForSum.Add(numbers[index]);
+                CalculateSubset(index + 1, numbersForSum);
+                numbersForSum.RemoveAt(numbersForSum.Count - 1);
+            }
         }
     }
 }
---
+++ 02. C# Advanced/01. Arrays/homework/Arrays/13. MergeSort/MergeSort.cs
@@ -0,0 +1,17 @@
+﻿namespace _13.MergeSort
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MergeSort
+    {
+        public static void Main()
+        {
+        }
+    }
+}
+
+// Write a program that sorts an array of integers using the Merge sort algorithm.
