eaeb5b42dcad6fdffd1b8766859694d7a25135ba
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-05-15 22:16:44 +0300

    added personal notes

--- 02. C# Advanced/01. Arrays/homework/Arrays/21. CombinationsOfSet/CombinationsOfSet.cs
+++ 02. C# Advanced/01. Arrays/homework/Arrays/21. CombinationsOfSet/CombinationsOfSet.cs
@@ -15,3 +15,6 @@
 }
 
 // Write a program that reads two numbers N and K and generates all the combinations of K distinct elements from the set [1..N].
+// Personal notes:
+// Combinations - counting without replacement and the order doesn't matter
+// n-objects, taken r at a time: n! / (n - r)! * r!
