b3905ebdaf85d8b9c56029d31e0aa7af8ab3a56e
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-05-23 18:22:55 +0300

    Advanced C# Exam preparation
    Added exercise 02. MovingLetters

--- 02. C# Advanced/ExamPreparation/CSharpPart2ExamPreparation/02. MovingLetters/MovingLetters.cs
+++ 02. C# Advanced/ExamPreparation/CSharpPart2ExamPreparation/02. MovingLetters/MovingLetters.cs
@@ -10,6 +10,29 @@
     {
         public static void Main()
         {
+            List<string> input = Console.ReadLine().Split().ToList();
+            StringBuilder result = new StringBuilder();
+            int inputMaxLenght = input.Max(x => x.Length);
+            for (int i = 0; i < inputMaxLenght; i++)
+            {
+                for (int j = 0; j < input.Count; j++)
+                {
+                    if (i < input[j].Length)
+                    {
+                        result.Append(input[j][input[j].Length - (1 + i)].ToString());
+                    }
+                }
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                char currentLetter = result[i];
+                int index = (i + ((char.ToLower(currentLetter) - 'a') + 1)) % result.Length;
+                result = result.Remove(i, 1);
+                result = result.Insert(index, currentLetter.ToString());
+            }
+
+            Console.WriteLine(result.ToString());
         }
     }
 }
