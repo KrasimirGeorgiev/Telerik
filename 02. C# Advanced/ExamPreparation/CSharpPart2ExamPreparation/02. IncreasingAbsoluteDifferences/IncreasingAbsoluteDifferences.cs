2133b5055359fd4515e0f42cdf6f5757b248151f
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-05-25 16:40:06 +0300

    Advanced C# exam preparation
    Added exercise 02. IncreasingAbsoluteDifferences

---
+++ 02. C# Advanced/ExamPreparation/CSharpPart2ExamPreparation/02. IncreasingAbsoluteDifferences/IncreasingAbsoluteDifferences.cs
@@ -0,0 +1,54 @@
+﻿namespace _02.IncreasingAbsoluteDifferences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class IncreasingAbsoluteDifferences
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                List<long> currentList = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+                List<long> absoluteDiffList = FindAbsoluteDifferenceSeq(currentList).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+                bool isLegit = true;
+                for (int j = 1; j < absoluteDiffList.Count; j++)
+                {
+                    long firstNumber = absoluteDiffList[j - 1];
+                    long secondNumber = absoluteDiffList[j];
+                    if (firstNumber > secondNumber || Math.Abs(firstNumber - secondNumber) > 1)
+                    {
+                        isLegit = false;
+                        break;
+                    }
+                }
+
+                Console.WriteLine(isLegit ? "True" : "False");
+            }
+        }
+
+        private static string FindAbsoluteDifferenceSeq(List<long> list)
+        {
+            string result = string.Empty;
+            for (int i = 1; i < list.Count; i++)
+            {
+                long firstNumber = list[i - 1];
+                long secondNumber = list[i];
+                result += Math.Abs(firstNumber - secondNumber).ToString();
+                if (i != list.Count - 1)
+                {
+                    result += " ";
+                }
+            }
+
+            return result;
+        }
+    }
+}
+
+// C# Part 2 - 2015/2016 @ 6 March 2015 - Evening
+// http://bgcoder.com/Contests/Practice/Index/223#1
