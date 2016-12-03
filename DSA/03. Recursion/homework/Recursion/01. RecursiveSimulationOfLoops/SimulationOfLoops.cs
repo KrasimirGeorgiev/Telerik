6a98827a61edd7cdcae4918832c4ff5171edb5db
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-12-03 13:13:39 +0200

    Recursion
    01a. Factrorial

---
+++ DSA/03. Recursion/homework/Recursion/01. RecursiveSimulationOfLoops/SimulationOfLoops.cs
@@ -0,0 +1,28 @@
+﻿namespace _01.RecursiveSimulationOfLoops
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SimulationOfLoops
+    {
+        public static void Main()
+        {
+
+        }
+
+        private static void PrintNestedLops(int n)
+        {
+            
+        }
+    }
+}
+
+// Write a recursive program that simulates the execution of n nested loopsfrom 1 to n.
+// Examples:
+//              1 1
+//  n= 2   ->   1 2
+//              2 1
+//              2 2
---
+++ DSA/03. Recursion/homework/Recursion/01a. Factorial/Factorial.cs
@@ -0,0 +1,24 @@
+﻿namespace _01a.Factorial
+{
+    using System;
+
+    public class Factorial
+    {
+        public static void Main()
+        {
+            int n = 20; // 432902008176640000
+            long factorial = CalculateFactorial(n);
+            Console.WriteLine(factorial);
+        }
+
+        private static long CalculateFactorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * CalculateFactorial(n - 1);
+        }
+    }
+}
