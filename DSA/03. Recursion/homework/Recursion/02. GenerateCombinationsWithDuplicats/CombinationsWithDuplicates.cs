8cf86a9e2c54c901c634c39fb418514ca2cf7574
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-12-05 17:56:11 +0200

    DSA - Recursion
    Added tasks 2) and 3) Generating combinations  with and without
    duplicates.

---
+++ DSA/03. Recursion/homework/Recursion/02. GenerateCombinationsWithDuplicats/CombinationsWithDuplicates.cs
@@ -0,0 +1,34 @@
+﻿namespace _02.GenerateCombinationsWithDuplicats
+{
+    using System;
+    using System.Text;
+
+    public class CombinationsWithDuplicates
+    {
+        public static void Main()
+        {
+            var sb = new StringBuilder();
+            var arr = new int[] { 1, 2, 3 };
+            Console.WriteLine(GenerateCombinations(0, arr.Length, 2, string.Empty, sb, arr));
+        }
+
+        private static StringBuilder GenerateCombinations(int counter, int n, int k, string str, StringBuilder sb, int[] arr)
+        {
+            if (counter == k)
+            {
+                return sb.AppendLine(str);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                sb = GenerateCombinations(counter + 1, n, k, str + " " + arr[i], sb, arr);
+            }
+
+            return sb;
+        }
+    }
+}
+
+// Write a recursive program for generating and printing all the combinations with duplicatesof k elements from n-element set.
+// Example:
+// n = 3, k= 2 → (1 1), (1 2), (1 3), (2 2), (2 3), (3 3)
---
+++ DSA/03. Recursion/homework/Recursion/03. GenerateCombinationsWithoutDuplicates/CombinationsWithoutDuplicates.cs
@@ -0,0 +1,33 @@
+﻿namespace _03.GenerateCombinationsWithoutDuplicates
+{
+    using System;
+    using System.Text;
+
+    public class CombinationsWithoutDuplicates
+    {
+        public static void Main()
+        {
+            var sb = new StringBuilder();
+            var arr = new int[] { 1, 2, 3, 4 };
+            Console.WriteLine(GenerateCombinations(0, 0, arr.Length, 2, string.Empty, sb, arr));
+        }
+
+        private static StringBuilder GenerateCombinations(int index, int counter, int n, int k, string str, StringBuilder sb, int[] arr)
+        {
+            if (counter == k)
+            {
+                return sb.AppendLine(str);
+            }
+
+            for (int i = index; i < n; i++)
+            {
+                sb = GenerateCombinations(i + 1, counter + 1, n, k, str + " " + arr[i], sb, arr);
+            }
+
+            return sb;
+        }
+    }
+}
+
+// Modify the previous program to skip duplicates:
+// n=4, k=2 → (1 2), (1 3), (1 4), (2 3), (2 4), (3 4)
