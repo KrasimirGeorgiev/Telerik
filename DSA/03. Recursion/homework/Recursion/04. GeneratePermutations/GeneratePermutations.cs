783182e2030463834b58afb6e089b5c740c5a9ec
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-12-06 19:48:41 +0200

    DSA - Recursion
    Added 04. GeneratePermutations

---
+++ DSA/03. Recursion/homework/Recursion/04. GeneratePermutations/GeneratePermutations.cs
@@ -0,0 +1,49 @@
+﻿namespace _04.GeneratePermutations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GeneratePermutations
+    {
+        public static int[] arr = new int[] { 1, 2, 3 };
+        public static void Main()
+        {
+            GeneratePermutationsUsingHeap(3, arr);
+        }
+
+        private static void GeneratePermutationsUsingHeap(int n, int[] arr)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(string.Join(", ", arr));
+            }
+            else
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    GeneratePermutationsUsingHeap(n - 1, arr);
+                    if (n % 2 == 0)
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[n - 1];
+                        arr[n - 1] = temp;
+                    }
+                    else
+                    {
+                        int temp = arr[0];
+                        arr[0] = arr[n - 1];
+                        arr[n - 1] = temp;
+                    }
+                }
+
+                GeneratePermutationsUsingHeap(n - 1, arr);
+            }
+        }
+    }
+}
+
+// Write a recursive program for generating and printing all permutations of the numbers 1, 2, ..., n for given integer number n.Example:
+// n= 3 → { 1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2},{3, 2, 1}
