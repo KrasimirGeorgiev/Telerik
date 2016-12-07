49150cee112dc1d4f680d1f4e0e0204aa76d8bc3
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-12-07 23:59:52 +0200

    DSA

---
+++ DSA/05. SortingAlgorithms/homework/project/SelectionSorter.cs
@@ -0,0 +1,34 @@
+﻿namespace SortingHomework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SelectionSorter<T> : ISorter<T> where T : IComparable<T>
+    {
+        public void Sort(IList<T> collection)
+        {
+            int len = collection.Count();
+            for (int i = 0; i < len - 1; i++)
+            {
+                int minElement = i;
+                for (int j = i + 1; j < len; j++)
+                {
+                    if (collection[j].CompareTo(collection[minElement]) < 0)
+                    {
+                        minElement = j;
+                    }
+                }
+
+                if (minElement != i)
+                {
+                    var swapVariable = collection[minElement];
+                    collection[minElement] = collection[i];
+                    collection[i] = swapVariable;
+                }
+            }
+        }
+    }
+}
