9f604a6a74d8820e5c8d0ac3f89479589fc67dee
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-12-09 15:27:25 +0200

    DSA - Searching and shuffling
    Added tasks for searching and shiffling

--- DSA/05. SortingAlgorithms/homework/project/Program.cs
+++ DSA/05. SortingAlgorithms/homework/project/Program.cs
@@ -31,23 +31,23 @@
             collection.Sort(new MergeSorter<int>());
             collection.PrintAllItemsOnConsole();
             Console.WriteLine();
-            //
-            //Console.WriteLine("Linear search 101:");
-            //Console.WriteLine(collection.LinearSearch(101));
-            //Console.WriteLine();
-            //
-            //Console.WriteLine("Binary search 101:");
-            //Console.WriteLine(collection.BinarySearch(101));
-            //Console.WriteLine();
-            //
-            //Console.WriteLine("Shuffle:");
-            //collection.Shuffle();
-            //collection.PrintAllItemsOnConsole();
-            //Console.WriteLine();
-            //
-            //Console.WriteLine("Shuffle again:");
-            //collection.Shuffle();
-            //collection.PrintAllItemsOnConsole();
+            
+            Console.WriteLine("Linear search 101:");
+            Console.WriteLine(collection.LinearSearch(101));
+            Console.WriteLine();
+            
+            Console.WriteLine("Binary search 101:");
+            Console.WriteLine(collection.BinarySearch(101));
+            Console.WriteLine();
+            
+            Console.WriteLine("Shuffle:");
+            collection.Shuffle();
+            collection.PrintAllItemsOnConsole();
+            Console.WriteLine();
+            
+            Console.WriteLine("Shuffle again:");
+            collection.Shuffle();
+            collection.PrintAllItemsOnConsole();
         }
     }
 }
--- DSA/05. SortingAlgorithms/homework/project/SortableCollection.cs
+++ DSA/05. SortingAlgorithms/homework/project/SortableCollection.cs
@@ -32,17 +32,58 @@
 
         public bool LinearSearch(T item)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i].CompareTo(item) == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public bool BinarySearch(T item)
         {
-            throw new NotImplementedException();
+            int low = 0;
+            int high = items.Count - 1;
+            int mid = (low + high) / 2;
+
+            while (low <= high && items[mid].CompareTo(item) != 0)
+            {
+                if (items[mid].CompareTo(item) < 0)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+
+                mid = (low + high) / 2;
+            }
+
+            if (low > high)
+            {
+                return false;
+            }
+
+            return true;
         }
 
+        // Durstenfeld's solution to Fisher–Yates shuffle with complexity O(n) instead of the O(n2) for the original Fisher–Yates shuffle
         public void Shuffle()
         {
-            throw new NotImplementedException();
+            int count = items.Count;
+            for (int i = 0; i < count - 2; i++)
+            {
+                for (int j = i; j < count; j++)
+                {
+                    var swapVar = items[i];
+                    items[i] = items[j];
+                    items[j] = swapVar;
+                }
+            }
         }
 
         public void PrintAllItemsOnConsole()
