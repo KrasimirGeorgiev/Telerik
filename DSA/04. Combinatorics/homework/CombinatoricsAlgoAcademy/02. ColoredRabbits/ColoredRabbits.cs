8cb931a7823b1d78ee61f9d005da64928debf00e
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-12-16 17:20:13 +0200

    DSA - Combinatorics
    Added tast 02. ColoredRabbits from Telerik Algo Academy October 2012

---
+++ DSA/04. Combinatorics/homework/CombinatoricsAlgoAcademy/02. ColoredRabbits/ColoredRabbits.cs
@@ -0,0 +1,43 @@
+﻿namespace _02.ColoredRabbits
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ColoredRabbits
+    {
+        public static void Main()
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int rabbitsMinCount = 0;
+
+            short lines = short.Parse(Console.ReadLine());
+            int answer = 0;
+            for (int i = 0; i < lines; i++)
+            {
+                answer = int.Parse(Console.ReadLine()) + 1;
+                if (dic.ContainsKey(answer))
+                {
+                    dic[answer]++;
+                }
+                else
+                {
+                    dic.Add(answer, 1);
+                }
+            }
+
+            foreach (int key in dic.Keys)
+            {
+                if (key >= dic[key])
+                {
+                    rabbitsMinCount += key;
+                }
+                else
+                {
+                    rabbitsMinCount += (float)dic[key] / key > dic[key] / key ? (dic[key] / key + 1) * key : (dic[key] / key) * key;
+                }
+            }
+
+            Console.WriteLine(rabbitsMinCount);
+        }
+    }
+}
