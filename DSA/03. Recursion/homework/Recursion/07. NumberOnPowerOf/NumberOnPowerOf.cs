ab8817e26576bb03d31ffde36931b6e5a98d7f48
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-12-16 16:00:20 +0200

    DSA - Combinatorics
    Added task 01. BinaryPassword of 10 from Algo Academy October 2012

--- DSA/03. Recursion/homework/Recursion/07. NumberOnPowerOf/NumberOnPowerOf.cs
+++ DSA/03. Recursion/homework/Recursion/07. NumberOnPowerOf/NumberOnPowerOf.cs
@@ -12,14 +12,37 @@
         {
         }
 
-        //private static int NumberOnPowerOf(int n, int power)
-        //{
-        //    if (power <= 1)
-        //    {
-        //        return power == 1 ? n : 1;
-        //    }
-        //      
-        //    return ()
-        //}
+        // private static int NumberOnPowerOf(int n, int power)
+        // {
+        //     var isEven = function(n) {
+        //         return n % 2 === 0;
+        //     };
+        // 
+        //     var isOdd = function(n) {
+        //         return !isEven(n);
+        //     };
+        // 
+        //     var power = function(x, n) {
+        //         println("Computing " + x + " raised to power " + n + ".");
+        //         // base case
+        //         // recursive case: n is negative 
+        //         // recursive case: n is odd
+        //         // recursive case: n is even
+        //     };
+        // 
+        //     var displayPower = function(x, n) {
+        //         println(x + " to the " + n + " is " + power(x, n));
+        //     };
+        // 
+        //     //displayPower(3, 0);
+        //     //Program.assertEqual(power(3, 0), 1);
+        //     //displayPower(3, 1);
+        //     //Program.assertEqual(power(3, 1), 3);
+        //     //displayPower(3, 2);
+        //     //Program.assertEqual(power(3, 2), 9);
+        //     //displayPower(3, -1);
+        //     //Program.assertEqual(power(3, -1), 1/3);
+        // }
     }
 }
+
---
+++ DSA/04. Combinatorics/homework/CombinatoricsAlgoAcademy/01. BinaryPasswords/BinaryPasswords.cs
@@ -0,0 +1,73 @@
+﻿namespace _01.BinaryPasswords
+{
+    using System;
+    using System.Numerics;
+
+    public class BinaryPasswords
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            var stars = CountStars(input);
+            PrintPermutations(stars);
+        }
+
+        private static short CountStars(string str)
+        {
+            byte counter = 0;
+            foreach (char symbol in str)
+            {
+                if (symbol == '*')
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        private static void PrintPermutations(short count)
+        {
+            if (count <= 7)
+            {
+                short number = 1;
+                for (short i = 0; i < count; i++)
+                {
+                    number *= 2;
+                }
+
+                Console.WriteLine(number);
+            }
+            else if (count <= 15)
+            {
+                ushort number = 1;
+                for (short i = 0; i < count; i++)
+                {
+                    number *= 2;
+                }
+
+                Console.WriteLine(number);
+            }
+            else if (count <= 53)
+            {
+                ulong number = 1;
+                for (short i = 0; i < count; i++)
+                {
+                    number *= 2;
+                }
+
+                Console.WriteLine(number);
+            }
+            else
+            {
+                BigInteger number = 1;
+                for (short i = 0; i < count; i++)
+                {
+                    number *= 2;
+                }
+
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
