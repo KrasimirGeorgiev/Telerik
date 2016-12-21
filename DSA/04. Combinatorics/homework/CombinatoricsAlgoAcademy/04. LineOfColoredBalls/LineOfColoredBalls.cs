a820817e2f20a946ee549a94cd28b23cd8474d23
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-12-21 14:20:49 +0200

    DSA - Combinatorics
    Added exersice 04. LineOfColouredBalls

---
+++ DSA/04. Combinatorics/homework/CombinatoricsAlgoAcademy/04. LineOfColoredBalls/LineOfColoredBalls.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace _04.LineOfColoredBalls
+{
+    class LineOfColoredBalls
+    {
+        static void Main()
+        {
+            Dictionary<int, int> ballsAndCount = new Dictionary<int, int>();
+            string input = Console.ReadLine();
+            foreach (int symbol in input)
+            {
+                if (ballsAndCount.ContainsKey(symbol))
+                {
+                    ballsAndCount[symbol]++; 
+                }
+                else
+                {
+                    ballsAndCount.Add(symbol, 1);
+                }
+            }
+
+            BigInteger top = Factorial(input.Length);
+            BigInteger bottom = 1;
+            foreach (char key in ballsAndCount.Keys)
+            {
+                bottom *= Factorial(ballsAndCount[key]);
+            }
+
+            Console.WriteLine(top / bottom);
+        }
+
+        private static BigInteger Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return (BigInteger)n * Factorial(n - 1);
+        }
+    }
+}
--- DSA/04. Combinatorics/homework/CombinatoricsAlgoAcademy/Tests/Test.cs
+++ DSA/04. Combinatorics/homework/CombinatoricsAlgoAcademy/Tests/Test.cs
@@ -6,7 +6,7 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    public class Tests
+    public class Test
     {
         public static void Main()
         {
