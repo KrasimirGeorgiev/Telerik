066048e18f51d33e0b0dd1045df6f9c496e6096e
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2016-12-21 19:20:20 +0200

    something

---
+++ C#OOP/01. Defining Classes - Part 1/homework/DefiningClasses Part1/DefiningClassesTask/GSM.cs
@@ -0,0 +1,6 @@
+﻿namespace DefiningClassesTask
+{
+    public class GSM
+    {
+    }
+}
---
+++ C#OOP/01. Defining Classes - Part 1/homework/DefiningClasses Part1/DefiningClassesTask/Start.cs
@@ -0,0 +1,16 @@
+﻿namespace DefiningClassesTask
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DefiningClassesTask
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
